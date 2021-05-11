afc4947ca52a19a1a7cf57b87049374c72e5cbfd
Phillip Johnston <phillip@embeddedartistry.com>
2021-05-11 16:03:30 -0700

    Import debug

--- lib/client/base.php
+++ lib/client/base.php
@@ -62,20 +62,24 @@ class Writing_On_GitHub_Base_Client {
         $status   = wp_remote_retrieve_header( $response, 'status' );
         $body     = json_decode( wp_remote_retrieve_body( $response ) );
 
+        WP_CLI::debug(sprintf(__('response: %s'), $response['body']));
+
         if ( '2' !== substr( $status, 0, 1 ) && '3' !== substr( $status, 0, 1 ) ) {
             WP_CLI::debug(sprintf(
-                    __( 'Method %s to endpoint %s failed with error: %s', 'writing-on-github' ),
+                    __( 'Method %s to endpoint %s failed with error: %s; status: %d', 'writing-on-github' ),
                     $method,
                     $endpoint,
-                    ( $body && $body->message ) ? $body->message : 'Unknown error'
+                    ( $body && isset($body->message) ) ? $body->message : 'Unknown error',
+                    $status
                 ));
             return new WP_Error(
                 strtolower( str_replace( ' ', '_', $status ) ),
                 sprintf(
-                    __( 'Method %s to endpoint %s failed with error: %s', 'writing-on-github' ),
+                    __( 'Method %s to endpoint %s failed with error: %s; status: %d', 'writing-on-github' ),
                     $method,
                     $endpoint,
-                    ( $body && $body->message ) ? $body->message : 'Unknown error'
+                    ( $body && isset($body->message) ) ? $body->message : 'Unknown error',
+                    $status
                 )
             );
         }
