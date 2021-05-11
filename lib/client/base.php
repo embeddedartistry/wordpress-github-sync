0fa9fde586b33f3624bbc99b85d1a18b48df3e6b
Phillip Johnston <phillip@embeddedartistry.com>
2021-05-11 15:53:49 -0700

    Import debug

--- lib/client/base.php
+++ lib/client/base.php
@@ -62,9 +62,10 @@ class Writing_On_GitHub_Base_Client {
         $status   = wp_remote_retrieve_header( $response, 'status' );
         $body     = json_decode( wp_remote_retrieve_body( $response ) );
 
-        WP_CLI::debug(vsprintf(("Response: %s %s %s"), $body));
+        WP_CLI::debug(vsprintf(("Response: %s %s %s %s"), $body));
 
         if ( '2' !== substr( $status, 0, 1 ) && '3' !== substr( $status, 0, 1 ) ) {
+             WP_CLI::debug(sprintf(__('Status: %d')));
             WP_CLI::debug(sprintf(
                     __( 'Method %s to endpoint %s failed with error: %s', 'writing-on-github' ),
                     $method,
